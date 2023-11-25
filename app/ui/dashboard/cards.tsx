f3342570fbef28381ab71de190f2357fe6cf5e41
aabor73 <aabor73@yandex.ru>
2023-11-25 13:17:36 +0300

    Three commit

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -15,18 +15,18 @@ const iconMap = {
 
 export default async function CardWrapper() {
   return (
-    <>
+    <div>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
+      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+      {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+      {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+      {/* <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
       /> */}
-    </>
+    </div>
   );
 }
 
